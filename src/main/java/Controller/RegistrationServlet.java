da6cd6ae7e95aa2b0d38366f07d6cde98d77d00e
krishna <krishnasingj137333@gmail.com>
2023-12-10 22:15:17 +0530

    render to main page

--- src/main/java/Controller/RegistrationServlet.java
+++ src/main/java/Controller/RegistrationServlet.java
@@ -28,7 +28,7 @@ public class RegistrationServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
+    	request.getRequestDispatcher("index.jsp").forward(request, response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
